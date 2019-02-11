173b234b48d5ed7a4a6245f716d369fab9a967f4
sandrex cabrales <sandrex.kami@gmail.com>
2019-02-12 00:57:16 +0800

    progress

--- thesis/js/CONTROLLER/adminController.js
+++ thesis/js/CONTROLLER/adminController.js
@@ -5,7 +5,7 @@ app.controller('adminController', function(
                                     
                                     ){
     $scope.prod = {};
-
+    $scope.message = {};                                    
 
     $scope.prodsubmit = function(){
         $scope.prod.name = $scope.prodname;
@@ -14,12 +14,16 @@ app.controller('adminController', function(
         $scope.prod.stock = $scope.prodstock;
         $scope.prod.category = $scope.prodcateg;
 
+        if($scope.prod.name == null || $scope.prod.descript == null || $scope.prod.price == null || 
+            $scope.prod.stock == null|| $scope.prod.category == null){
+                $scope.message = {nullmsg:'Field Empty'};
+        }
         var promise = adminFactory.addProd($scope.prod);
             promise.then(function(data){
                 alert("success");
             })
             .then(null, function(data){
-                alert("error");
+                
             });
     };
 
